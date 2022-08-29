e9d0f51c5a9618aa59c8b48e7697d247d7e50872
Michael Bestas <mkbestas@lineageos.org>
2022-08-30 02:59:07 +0300

    config: Remove TARGET_NEEDS_NETD_DIRECT_CONNECT_RULE
    This hasn't been ported after lineage-18.1
    
    Change-Id: I2946fdb08c09a3bfd238d65103869dc89ca2b558
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -28,7 +28,6 @@ SOONG_CONFIG_lmodroidGlobalVars += \
     bootloader_message_offset \
     disable_postrender_cleanup \
     needs_camera_boottime \
-    needs_netd_direct_connect_rule \
     target_init_vendor_lib \
     target_ld_shim_libs \
     target_process_sdk_version_override \
@@ -56,7 +55,6 @@ endif
 # Soong bool variables
 SOONG_CONFIG_lmodroidGlobalVars_disable_postrender_cleanup := $(TARGET_DISABLE_POSTRENDER_CLEANUP)
 SOONG_CONFIG_lmodroidGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
-SOONG_CONFIG_lmodroidGlobalVars_needs_netd_direct_connect_rule := $(TARGET_NEEDS_NETD_DIRECT_CONNECT_RULE)
 SOONG_CONFIG_lmodroidGlobalVars_uses_egl_display_array := $(TARGET_USES_EGL_DISPLAY_ARRAY)
 SOONG_CONFIG_lmodroidNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
 SOONG_CONFIG_lmodroidNvidiaVars_uses_nv_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
