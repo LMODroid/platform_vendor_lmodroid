7047fadb2a27ccfe902fa983d8b37843ac286c78
Tim Zimmermann <tim@linux4.de>
2023-03-14 07:05:52 +0100

    config: Inherit window_extensions.mk
    * Instead of explicitly building androidx.window.extensions
    
    Change-Id: I107ef8ef49eab77d91aa06fe3ade55eeaae83c71
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

--- config/common_mini_tablet.mk
+++ config/common_mini_tablet.mk
@@ -1,9 +1,10 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/window_extensions.mk)
+
 # Inherit mini common LMODroid stuff
 $(call inherit-product, vendor/lmodroid/config/common_mini.mk)
 
 # Required packages
 PRODUCT_PACKAGES += \
-    androidx.window.extensions \
     LatinIME
 
 $(call inherit-product, vendor/lmodroid/config/telephony.mk)
