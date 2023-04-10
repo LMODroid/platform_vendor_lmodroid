e3572be8edac1e06eef1f05d4f07108ddad59455
Erfan Abdi <erfangplus@gmail.com>
2023-04-10 16:40:53 +0330

    lmodroid: Suppress modules check for com.android.art
    Change-Id: I76be2bebd183398a648551f9068d7b1c1031dfca

--- config/common.mk
+++ config/common.mk
@@ -92,7 +92,7 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 ifneq ($(TARGET_DISABLE_EPPE),true)
 # Require all requested packages to exist
-$(call enforce-product-packages-exist-internal,$(wildcard device/*/$(LMODROID_BUILD)/$(TARGET_PRODUCT).mk),product_manifest.xml rild Calendar Launcher3 Launcher3Go Launcher3QuickStep Launcher3QuickStepGo android.hidl.memory@1.0-impl.vendor vndk_apex_snapshot_package)
+$(call enforce-product-packages-exist-internal,$(wildcard device/*/$(LMODROID_BUILD)/$(TARGET_PRODUCT).mk),product_manifest.xml rild Calendar Launcher3 Launcher3Go Launcher3QuickStep Launcher3QuickStepGo android.hidl.memory@1.0-impl.vendor vndk_apex_snapshot_package com.android.art)
 endif
 
 # Bootanimation
