54f4aceaea223a6f358837cdc55ed5f3b25b1a10
Erfan Abdi <erfangplus@gmail.com>
2023-04-11 12:27:02 +0330

    lmodroid: Keep out backuptool from user builds
    Change-Id: Id384ffd3fa4e540455a726e038c26bd837602808

--- config/common.mk
+++ config/common.mk
@@ -25,6 +25,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.sys.strictmode.disable=true
 endif
 
 # Backup Tool
+ifneq ($(TARGET_BUILD_VARIANT),user)
 PRODUCT_COPY_FILES += \
     vendor/lmodroid/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
     vendor/lmodroid/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
@@ -44,7 +45,6 @@ PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
     system/bin/backuptool_ab.functions \
     system/bin/backuptool_postinstall.sh
 
-ifneq ($(TARGET_BUILD_VARIANT),user)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.ota.allow_downgrade=true
 endif
