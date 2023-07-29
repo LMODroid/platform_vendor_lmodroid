37c056e60d4cff16175cfa63fc3b04bb6baef949
Nolen Johnson <johnsonnolen@gmail.com>
2023-07-29 16:48:04 -0400

    lineage: config: Allow devices without Cameras to opt-out of Aperture
    Change-Id: I6feb538153b13cd1825f37ee7a45ebb046b12ec0
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

--- config/common.mk
+++ config/common.mk
@@ -148,7 +148,6 @@ PRODUCT_PACKAGES += \
 
 # System apps
 PRODUCT_PACKAGES += \
-    Aperture \
     Etar \
     ExactCalculator \
     GameSpace \
@@ -156,6 +155,11 @@ PRODUCT_PACKAGES += \
     PdfViewer \
     Recorder \
     Seedvault
+
+ifneq ($(PRODUCT_NO_CAMERA),true)
+PRODUCT_PACKAGES += \
+    Aperture
+endif
 
 # TouchGestures
 PRODUCT_PACKAGES += \
