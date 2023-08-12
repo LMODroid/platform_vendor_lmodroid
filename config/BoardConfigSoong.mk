fb523bee6a1fbb9fedd6df78bac8d14afe5bdd55
Erfan Abdi <erfangplus@gmail.com>
2023-08-12 15:00:49 +0330

    soong: Add mtk surfaceflinger soong config variable
    Change-Id: I713a415c4c17ae381588fc74e3e8d1905cc4664d

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -56,6 +56,10 @@ SOONG_CONFIG_lmodroidQcomVars += \
     supports_extended_compress_format \
     uses_pre_uplink_features_netmgrd
 
+SOONG_CONFIG_NAMESPACES += lmodroidMtkVars
+SOONG_CONFIG_lmodroidMtkVars += \
+    has_mtk_surfaceflinger
+
 # Only create display_headers_namespace var if dealing with UM platforms to avoid breaking build for all other platforms
 ifneq ($(filter $(UM_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
 SOONG_CONFIG_lmodroidQcomVars += \
@@ -73,6 +77,7 @@ SOONG_CONFIG_lmodroidNvidiaVars_uses_nv_enhancements := $(NV_ANDROID_FRAMEWORK_E
 SOONG_CONFIG_lmodroidQcomVars_qti_vibrator_use_effect_stream := $(TARGET_QTI_VIBRATOR_USE_EFFECT_STREAM)
 SOONG_CONFIG_lmodroidQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_lmodroidQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
+SOONG_CONFIG_lmodroidMtkVars_has_mtk_surfaceflinger := $(TARGET_HAS_MTK_SURFACEFLINGER)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0
