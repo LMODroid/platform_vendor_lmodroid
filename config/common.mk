87cf83f16ecd7182a760911ce67007eef2f9bdb3
Bruno Martins <bgcngm@gmail.com>
2023-10-03 11:38:15 +0100

    config: Add LMODroid specific languages via CUSTOM_LOCALES
    Change-Id: If9a8caad7a07ae1c133e0d05c71a8cacc64de92e
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

--- config/common.mk
+++ config/common.mk
@@ -283,6 +283,11 @@ PRODUCT_PACKAGES += \
     NetworkStackOverlay
 
 # Translations
+CUSTOM_LOCALES += \
+    ast_ES \
+    gd_GB \
+    cy_GB
+
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/crowdin/overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/crowdin/overlay
 
