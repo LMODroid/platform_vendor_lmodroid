dc0ac31ae617295fce15db1f7c27d9c062ef31dc
Bruno Martins <bgcngm@gmail.com>
2024-01-19 22:45:27 +0000

    config/common_car: Remove car_product inheritance
    Should be done in the device trees, like Google does and all devices
    that we support with different build types (mobile and ATV).
    
    The only requirement is to call the makefile before vendor/lineage one
    so that PRODUCT_IS_AUTOMOTIVE is set before inheriting from Lineage
    common stuff. That allows to skip some undesired package if needed
    without having to move those out from common makefile and duplicate
    into common_car and common_tv makefiles.
    
    Change-Id: Id663f48f3d484f415ff04869c52073fbc6b87148

--- config/common_car.mk
+++ config/common_car.mk
@@ -3,6 +3,3 @@ $(call inherit-product, vendor/lmodroid/config/common.mk)
 
 # Inherit LMODroid car device tree
 $(call inherit-product, device/lmodroid/car/lmodroid_car.mk)
-
-# Inherit the main AOSP car makefile that turns this into an Automotive build
-$(call inherit-product, packages/services/Car/car_product/build/car.mk)
