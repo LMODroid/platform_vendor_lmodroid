88f9f77be11541c5be6ceaa14629ae5e7027afa4
Michael Bestas <mkbestas@lineageos.org>
2024-03-23 01:54:48 +0200

    config: Include AudioTv.mk on atv
    Change-Id: Id6cf181c35032b44ab705b62bc9600f5b4f171bb

--- config/common_tv.mk
+++ config/common_tv.mk
@@ -1,6 +1,9 @@
 # Inherit common LMODroid stuff
 $(call inherit-product, vendor/lmodroid/config/common.mk)
 
+# Include AOSP audio files
+$(call inherit-product-if-exists, frameworks/base/data/sounds/AudioTv.mk)
+
 # Inherit LMODroid atv device tree
 $(call inherit-product, device/lmodroid/atv/lmodroid_atv.mk)
 
