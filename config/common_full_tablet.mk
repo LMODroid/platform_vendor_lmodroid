2ee6d6f77b756c3731d90d22d9830d482a36d4b0
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-07-21 21:04:37 +0200

    config: tablet: Deduplicate full tablet configuration
    Change-Id: I12cc09d4fca28d063cea76301de287526bfec570

--- config/common_full_tablet.mk
+++ config/common_full_tablet.mk
@@ -1,10 +1,7 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/window_extensions.mk)
-
 # Inherit mobile full common LMODroid stuff
 $(call inherit-product, vendor/lmodroid/config/common_mobile_full.mk)
 
-# Settings
-PRODUCT_PRODUCT_PROPERTIES += \
-    persist.settings.large_screen_opt.enabled=true
+# Inherit full tablet common LMODroid stuff
+$(call inherit-product, vendor/lmodroid/config/full_tablet.mk)
 
 $(call inherit-product, vendor/lmodroid/config/telephony.mk)
