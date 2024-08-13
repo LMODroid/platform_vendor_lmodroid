LineageOS/android_vendor_lineage

config: Move LatinIME stuff to common_mobile*.mk
Change-Id: I5697e1cef84972a6a7f76b8c065e575e1ba17ef8

config/common_mobile_full.mk
@@ -35,3 +35,7 @@ PRODUCT_PACKAGES += \
     fonts_customization.xml \
     FontLatoOverlay \
     FontRubikOverlay
+
+# Include Lineage LatinIME dictionaries
+PRODUCT_PACKAGE_OVERLAYS += vendor/lineage/overlay/dictionaries
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/lineage/overlay/dictionaries

LMODroid/platform_vendor_lmodroid

config/common_mobile_full.mk
@@ -23,3 +23,7 @@ PRODUCT_PACKAGES += \
 # Recorder
 PRODUCT_PACKAGES += \
     Recorder
+
+# Include LMODroid LatinIME dictionaries
+PRODUCT_PACKAGE_OVERLAYS += vendor/lmodroid/overlay/dictionaries
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/lmodroid/overlay/dictionaries