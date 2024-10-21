ecd4eda65d4fd1bb620985762e20dde563dca70e
Dhina17 <dhinalogu@gmail.com>
2024-10-22 01:12:36 +0530

    lmodroid: Split cust envsetup help to a separate file
    Change-Id: Iea6c539a3ccbc1dcf8d637339e826d4b335c6d23

--- cust/build/cust.sh
+++ cust/build/cust.sh
@@ -1,12 +1,3 @@
-# Additional help function
-# Print extra information when user types "hmm"
-__customer_help() {
-    cat <<EOF
-Additional functions:
-
-EOF
-}
-
 # Customer build prefix
 # If undefined, customer can provide custom __customer_check_product
 # If empty, prefix is disabled
