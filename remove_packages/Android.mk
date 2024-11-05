6eba91dad6f6d38572da97c55615db3bd0aa64c1
Mohammad Hasan Keramat J <ikeramat@libremobileos.com>
2024-11-05 21:43:06 +0330

    lmodroid: Setup LMORemovePackagesMinimal
    Change-Id: If39adcd53e61cadfe7fe0a25cfd1ecd04e190a8b

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -10,3 +10,12 @@ LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := LMORemovePackagesMinimal
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := Gramophone F-Droid F-DroidPrivilegedExtension UdfpsAnimations NumberHub FontAlbertSansOverlay FontVazirmatnOverlay Backgrounds GameSpace ParallelSpace LMOPdfViewer OpenEUICC Ripple QuickSearchBox Recorder TouchGestures Glimpse Etar
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)
