a6e8e1b6b334526bcfe92dfc0a1028982a51b085
Mohammad Hasan Keramat J <ikeramat@libremobileos.com>
2024-12-07 14:05:11 +0330

    lmodroid: Build LMOStats
    Change-Id: I849f7eaf1d053e04bbcac82ac518506661c9e024
    (cherry picked from commit 3a63d34a6fdd40712e81d9a19ef8df71d8739215)

--- config/common.mk
+++ config/common.mk
@@ -274,6 +274,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     rsync
 
+# Stats
+PRODUCT_PACKAGES += \
+    LMOStats
+
 # Storage manager
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.storage_manager.enabled=true
