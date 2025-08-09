65a25d05352d4d31d5bd3a3d864b490d1c0ae4c3
LuK1337 <priv.luk@gmail.com>
2025-08-09 11:08:33 +0200

    Reapply "lmodroid: Add framework compatibility matrix for Lineage HALs"
    It's time to fix your devices.
    
    This reverts commit 9887e1f75199cb37af46f13b53b3b5a8464d8466.
    
    Change-Id: I78da6340f38b93fdc4c3694cb8f431f387d16c0d

--- config/common.mk
+++ config/common.mk
@@ -398,6 +398,8 @@ CUSTOM_LOCALES += \
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/crowdin/overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/crowdin/overlay
 
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lmodroid/config/device_framework_matrix.xml
+
 -include vendor/lmodroid-priv/keys/keys.mk
 
 include $(CUSTOMER_VENDOR_DIR)/build/version.mk
