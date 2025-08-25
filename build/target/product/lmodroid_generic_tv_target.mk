4629d239233b0cea67f0681bdbd2bbaaf5584e9d
Yumi Yukimura <me.cafebabe@gmail.com>
2025-08-25 23:30:27 +0800

    lmodroid_generic_*target: Disable soong defined system image for now
    We don't support it yet.
    
    Change-Id: Ic5cfa50ff66bfee723b74d4e5511582b42a2eeca

--- build/target/product/lmodroid_generic_tv_target.mk
+++ build/target/product/lmodroid_generic_tv_target.mk
@@ -18,3 +18,6 @@ $(call inherit-product, vendor/lmodroid/config/common_full_tv.mk)
 
 # Allow building otatools
 TARGET_FORCE_OTA_PACKAGE := true
+
+# Disable soong defined system image for now
+USE_SOONG_DEFINED_SYSTEM_IMAGE := false
