LineageOS/android_vendor_lineage

bootanimation: Convert to Android.bp
Change-Id: Id82308522c3cf25d2fb89a1a18b78191c89c4931

config/BoardConfigSoong.mk
@@ -19,6 +19,16 @@ EXPORT_TO_SOONG := \
 $(call add_soong_config_namespace,lineageVarsPlugin)
 $(foreach v,$(EXPORT_TO_SOONG),$(eval $(call add_soong_config_var,lineageVarsPlugin,$(v))))
 
+# Bootanimation
+TARGET_BOOTANIMATION_HALF_RES ?= false
+$(call soong_config_set,lineage_bootanimation,height,$(TARGET_SCREEN_HEIGHT))
+$(call soong_config_set,lineage_bootanimation,width,$(TARGET_SCREEN_WIDTH))
+$(call soong_config_set,lineage_bootanimation,half_res,$(TARGET_BOOTANIMATION_HALF_RES))
+
+ifneq ($(TARGET_BOOTANIMATION),)
+$(call soong_config_set,lineage_bootanimation,prebuilt_file,$(TARGET_BOOTANIMATION))
+endif
+
 # Camera
 ifneq ($(TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED),)
     $(warning TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED is deprecated, please migrate to soong_config_set,camera,override_format_from_reserved)

LMODroid/platform_vendor_lmodroid

config/BoardConfigSoong.mk
@@ -15,6 +15,16 @@ EXPORT_TO_SOONG := \
 $(call add_soong_config_namespace,lmodroidVarsPlugin)
 $(foreach v,$(EXPORT_TO_SOONG),$(eval $(call add_soong_config_var,lmodroidVarsPlugin,$(v))))
 
+# Bootanimation
+TARGET_BOOTANIMATION_HALF_RES ?= false
+$(call soong_config_set,lmodroid_bootanimation,height,$(TARGET_SCREEN_HEIGHT))
+$(call soong_config_set,lmodroid_bootanimation,width,$(TARGET_SCREEN_WIDTH))
+$(call soong_config_set,lmodroid_bootanimation,half_res,$(TARGET_BOOTANIMATION_HALF_RES))
+
+ifneq ($(TARGET_BOOTANIMATION),)
+$(call soong_config_set,lmodroid_bootanimation,prebuilt_file,$(TARGET_BOOTANIMATION))
+endif
+
 # Camera
 ifneq ($(TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED),)
     $(warning TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED is deprecated, please migrate to soong_config_set,camera,override_format_from_reserved)