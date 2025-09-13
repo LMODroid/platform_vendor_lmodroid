b6bb6a58284a79e0ed56c675d96aec73856f7750
Dhina17 <dhinalogu@gmail.com>
2025-09-13 15:47:06 +0530

    remove_packages: Remove lmo dynamic walls and clock plugin in minimal variant
    Change-Id: Ie46c39d94d0733241329babe9471c9ac25fe353a

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -14,7 +14,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := LMORemovePackagesMinimal
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Gramophone F-Droid F-DroidPrivilegedExtension UdfpsAnimations NumberHub FontAlbertSansOverlay FontVazirmatnOverlay Backgrounds GameSpace ParallelSpace LMOPdfViewer OpenEUICC Ripple QuickSearchBox Recorder TouchGestures Glimpse Etar
+LOCAL_OVERRIDES_PACKAGES := Gramophone F-Droid F-DroidPrivilegedExtension UdfpsAnimations NumberHub FontAlbertSansOverlay FontVazirmatnOverlay Backgrounds GameSpace ParallelSpace LMOPdfViewer OpenEUICC Ripple QuickSearchBox Recorder TouchGestures Glimpse Etar LMODynamicWallpaper LMOSystemUIClock
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
