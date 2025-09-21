f3fb6bbabcff9ae56873190c5919789f5c966636
Mohammad Hasan Keramat J <ikeramat@libremobileos.com>
2025-09-21 17:14:17 +0330

    lmodroid: Add OrganicMaps remove packages
    Change-Id: I3ef55ea317c670ddbea70ea33ab39709e814b63e

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := LMORemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Gramophone F-Droid
+LOCAL_OVERRIDES_PACKAGES := Gramophone F-Droid OrganicMaps
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
@@ -14,7 +14,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := LMORemovePackagesMinimal
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Gramophone F-Droid F-DroidPrivilegedExtension UdfpsAnimations NumberHub FontAlbertSansOverlay FontVazirmatnOverlay Backgrounds GameSpace ParallelSpace LMOPdfViewer OpenEUICC Ripple QuickSearchBox Recorder TouchGestures Glimpse Etar LMODynamicWallpaper LMOSystemUIClock
+LOCAL_OVERRIDES_PACKAGES := Gramophone F-Droid F-DroidPrivilegedExtension UdfpsAnimations NumberHub FontAlbertSansOverlay FontVazirmatnOverlay Backgrounds GameSpace ParallelSpace LMOPdfViewer OpenEUICC Ripple QuickSearchBox Recorder TouchGestures Glimpse Etar LMODynamicWallpaper LMOSystemUIClock OrganicMaps
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
