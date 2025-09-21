baddbef12d534a4e9e678ccc1a767fb2be6902ae
Mohammad Hasan Keramat J <ikeramat@libremobileos.com>
2025-09-21 17:16:52 +0330

    lmodroid: Add UdfpsAnimations to LMORemovePackages
    Change-Id: I20b6e4c9ea3628a78f34c3538a51ede0e1ea825e

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := LMORemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Gramophone F-Droid OrganicMaps
+LOCAL_OVERRIDES_PACKAGES := Gramophone F-Droid OrganicMaps UdfpsAnimations
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
