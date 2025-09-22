4382a21bd2ef2ef519af3b9eca0fb83abaa8520c
Mohammad Hasan Keramat J <ikeramat@libremobileos.com>
2025-09-22 07:15:43 +0330

    lmodroid: Add more packages to LMORemovePackages
    Change-Id: I64c2134df72667b807635778ac3739585d2c7679

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := LMORemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Gramophone F-Droid OrganicMaps UdfpsAnimations
+LOCAL_OVERRIDES_PACKAGES := Gramophone F-Droid OrganicMaps UdfpsAnimations NumberHub FontAlbertSansOverlay FontVazirmatnOverlay Camelot QuickSearchBox Recorder TouchGestures
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
@@ -14,7 +14,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := LMORemovePackagesMinimal
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Gramophone F-Droid F-DroidPrivilegedExtension UdfpsAnimations NumberHub FontAlbertSansOverlay FontVazirmatnOverlay Backgrounds GameSpace ParallelSpace LMOPdfViewer OpenEUICC Ripple QuickSearchBox Recorder TouchGestures Glimpse Etar LMODynamicWallpaper LMOSystemUIClock OrganicMaps
+LOCAL_OVERRIDES_PACKAGES := Gramophone F-Droid F-DroidPrivilegedExtension UdfpsAnimations NumberHub FontAlbertSansOverlay FontVazirmatnOverlay Backgrounds GameSpace ParallelSpace Camelot OpenEUICC Ripple QuickSearchBox Recorder TouchGestures Glimpse Etar LMODynamicWallpaper LMOSystemUIClock OrganicMaps
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
