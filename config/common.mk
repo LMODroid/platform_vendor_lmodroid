76efc85edcf3505d5bf7b3d476fd97195e94186b
Mohammad Hasan Keramat J <ikeramat@libremobileos.com>
2025-09-22 22:46:21 +0330

    lmodroid: Add blueline and bonito to LMORemovePackages
    Change-Id: I4c4d083b0c6a80f630027e2adf4a16228bad5161

--- config/common.mk
+++ config/common.mk
@@ -188,7 +188,7 @@ PRODUCT_PACKAGES += \
 
 endif
 
-ifneq ($(filter %_lemonades %_kebab %_nairo %_alioth %_cebu %_crosshatch %_guam %_guamp %_martini %_sargo,$(TARGET_PRODUCT)),)
+ifneq ($(filter %_lemonades %_kebab %_nairo %_alioth %_cebu %_crosshatch %_guam %_guamp %_martini %_sargo %_blueline %_bonito,$(TARGET_PRODUCT)),)
 PRODUCT_PACKAGES += LMORemovePackages
 endif
 
