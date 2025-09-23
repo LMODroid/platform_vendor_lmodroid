47b4dbe186588458997fbc6d6d221bc0d5c50a15
Mohammad Hasan Keramat J <ikeramat@libremobileos.com>
2025-09-24 00:08:26 +0330

    lmodroid: Disable blur on LMORemovePackagesMinimal targets
    Change-Id: I1d8b70fae614fa859ed2632a231479f846574b5f

--- config/common.mk
+++ config/common.mk
@@ -193,6 +193,7 @@ PRODUCT_PACKAGES += LMORemovePackagesMinimal
 PRODUCT_NO_CAMERA := true
 TARGET_FACE_UNLOCK_OPTOUT := true
 TARGET_WITHOUT_DESKTOPMODE := true
+TARGET_NOT_USES_BLUR := true
 endif
 
 ifneq ($(filter %_pro1 %_pro1x %_l01k %_joan %_judyp %_judypn %_caymanslm %_racer %_berlin %_pstar %_berlna %_dubai %_tundra %_eqs %_rtwo %_nio %_xpeng %_nx606j %_d1 %_d1x %_d2s %_d2x %_beyond1lte %_beyondx %_beyond2lte %_beyond0lte %_r8q %_cupid %_zeus %_mayfly %_thor %_nuwa %_umi %_cmi %_thyme %_tucana %_zahedan,$(TARGET_PRODUCT)),)
