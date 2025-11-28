16e1eca9af085e73bb062a04e6eba10df5373730
Nick Reuter <nreuter85@gmail.com>
2025-11-27 21:08:46 -0600

    lmodroid: Move device_framework_matrix.xml to hardware/lineage/interfaces
    Follows AOSP structure that exists in hardware/interfaces.
    
    Change-Id: I3ebbd53d4e9920e8a56afd78e18022b06a7b3826

--- config/common.mk
+++ config/common.mk
@@ -154,6 +154,10 @@ PRODUCT_PACKAGES += \
     bootanimation.zip \
     bootanimation-dark.zip
 
+# Lineage interfaces
+PRODUCT_PACKAGES += \
+    framework_compatibility_matrix.lineage.xml
+
 # Backgrounds
 PRODUCT_PACKAGES += \
     Backgrounds
@@ -408,8 +412,6 @@ CUSTOM_LOCALES += \
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/crowdin/overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/crowdin/overlay
-
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lmodroid/config/device_framework_matrix.xml
 
 -include vendor/lmodroid-priv/keys/keys.mk
 
